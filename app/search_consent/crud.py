1d8ce00369171665f659b3d2d7e09290eeb7d3c5
luke <luke.waninger@gmail.com>
2019-02-11 14:27:35 -0800

    prior to removing ssl

--- app/search_consent/crud.py
+++ app/search_consent/crud.py
@@ -1,15 +1,29 @@
 import datetime as dt
-
-from flask import Blueprint, redirect, render_template, request, session, url_for
+from functools import wraps
+from flask import Blueprint, current_app, redirect, render_template, request, session, url_for
 from pytz import timezone
 
-import app.config as secrets
 import app.model as model
 from app.search_consent import oauth2
 
 crud = Blueprint('crud', __name__)
 
 
+def ssl_required(fn):
+    @wraps(fn)
+    def decorated_view(*args, **kwargs):
+        if current_app.config.get('SSL'):
+            if request.is_secure:
+                return fn(*args, **kwargs)
+            else:
+                return redirect(request.url.replace('http://', 'https://'))
+
+        return fn(*args, **kwargs)
+
+    return decorated_view
+
+
+@ssl_required
 @crud.route('/download', methods=['GET', 'POST'])
 @oauth2.required
 def download():
@@ -24,16 +38,18 @@ def download():
         data['guid_counter'] = model.get_next_guid_counter(data['guid'])
 
         data['email'] = session['profile']['emails'][0]['value']
-
+        data['first_name'] = session['profile']['name']['givenName']
+        data['last_name'] = session['profile']['name']['familyName']
+        data['gender'] = session['profile']['gender']
         now = dt.datetime.now(timezone('US/Pacific'))
-        data['consent_dt'] = now.strftime(secrets.DTFORMAT)
+        data['consent_dt'] = now
 
         data['credentials'] = session['google_oauth2_credentials']
 
         model.put_consent_to_synapse(data)
         model.add_task(data)
 
-        return redirect(url_for('.thanks'))
+        return redirect('https://takeout.google.com')
 
 
 @crud.route('/thanks', methods=['GET'])
